a7a57f2a037919df1a5f243449ca57e53021c1ff
PC Nguyen <pnguyen@reputation.com>
2014-12-16 15:42:59 -0800

    implement data source

---
+++ kiosk/UI/RPKMenuDataSource.h
@@ -0,0 +1,42 @@
+//
+//  RPKMenuDataSource.h
+//  kiosk
+//
+//  Created by PC Nguyen on 12/16/14.
+//  Copyright (c) 2014 Reputation. All rights reserved.
+//
+
+#import "ULViewDataSource.h"
+
+@interface RPKMenuItem : NSObject
+
+/**
+ *  The URL to be loaded in webview
+ */
+@property (nonatomic, strong) NSURL *itemURL;
+
+/**
+ *  The logo image name
+ */
+@property (nonatomic, strong) NSString *imageName;
+
+/**
+ *  The title for item
+ */
+@property (nonatomic, strong) NSString *itemTitle;
+
+/**
+ *  A brief description
+ */
+@property (nonatomic, strong) NSString *itemDetail;
+
+@end
+
+@interface RPKMenuDataSource : ULViewDataSource
+
+/**
+ *  An array of RPKMenuItem
+ */
+@property (nonatomic, strong) NSArray *menuItems;
+
+@end
---
+++ kiosk/UI/RPKMenuDataSource.m
@@ -0,0 +1,43 @@
+//
+//  RPKMenuDataSource.m
+//  kiosk
+//
+//  Created by PC Nguyen on 12/16/14.
+//  Copyright (c) 2014 Reputation. All rights reserved.
+//
+
+#import "RPKMenuDataSource.h"
+
+@implementation RPKMenuItem
+
+@end
+
+@implementation RPKMenuDataSource
+
+- (void)loadData
+{
+	NSMutableArray *menuItems = [NSMutableArray array];
+	
+	//--loading Kiosk
+	RPKMenuItem *kioskItem = [[RPKMenuItem alloc] init];
+	kioskItem.itemURL = [NSURL URLWithString:@"http://google.com"];
+	kioskItem.imageName = @"reputation_icon.png";
+	kioskItem.itemTitle = @"Kiosk Review";
+	kioskItem.itemDetail = @"Leave a review for this business on Reputation.com. Leave a review for this business on Reputation.com.Leave a review for this business on Reputation.com. Leave a review for this business on Reputation.com. Leave a review for this business on Reputation.com.";
+	[menuItems addObject:kioskItem];
+	
+	//--loading Google Plus
+	RPKMenuItem *googlePlusItem = [[RPKMenuItem alloc] init];
+	NSString *redirectURL = @"https://plus.google.com/_/widget/render/localreview?hl=en&origin=https%3A%2F%2Fplus.google.com&placeid=2221140452266056572&source=lo-pp&jsh=m%3B%2F_%2Fscs%2Fapps-static%2F_%2Fjs%2Fk%3Doz.gapi.en.iBmVlTSSvHg.O%2Fm%3D__features__%2Frt%3Dj%2Fd%3D1%2Ft%3Dzcms%2Frs%3DAGLTcCNDdqzVHtQZOJq6TKMfnKZKIZX46Q#rpctoken=359128756&_methods=onSuccess%2ConCancel%2ConError%2C_ready%2C_close%2C_open%2C_resizeMe%2C_renderstart&id=I3_1418679196082&parent=https%3A%2F%2Fplus.google.com&pfname=";
+	redirectURL = [redirectURL stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+	NSString *loginURL = [NSString stringWithFormat:@"https://accounts.google.com/ServiceLogin?passive=1209600&continue=%@",redirectURL];
+	googlePlusItem.itemURL = [NSURL URLWithString:loginURL];
+	googlePlusItem.imageName = @"gplus_icon.png";
+	googlePlusItem.itemTitle = @"Google Review";
+	googlePlusItem.itemDetail = @"Leave a review for this business on Google Plus. Leave a review for this business on Google Plus. Leave a review for this business on Google Plus. Leave a review for this business on Google Plus. Leave a review for this business on Google Plus. Leave a review for this business on Google Plus.";
+	[menuItems addObject:googlePlusItem];
+	
+	self.menuItems = menuItems;
+}
+
+@end
