8695ed9bf0ac214413fb71504d543b7a5650d588
PC Nguyen <pnguyen@reputation.com>
2014-12-16 10:05:22 -0800

    Initial Commit

---
+++ kioskTests/kioskTests.m
@@ -0,0 +1,40 @@
+//
+//  kioskTests.m
+//  kioskTests
+//
+//  Created by PC Nguyen on 12/16/14.
+//  Copyright (c) 2014 Reputation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface kioskTests : XCTestCase
+
+@end
+
+@implementation kioskTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
