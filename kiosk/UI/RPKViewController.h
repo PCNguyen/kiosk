5677487fbee5568f04af76be8a2a8c6dd091d053
PC Nguyen <pnguyen@reputation.com>
2014-12-16 11:33:24 -0800

    adding wrapper class, pod in TOWebViewController

---
+++ kiosk/UI/RPKViewController.h
@@ -0,0 +1,13 @@
+//
+//  RPKViewController.h
+//  kiosk
+//
+//  Created by PC Nguyen on 12/16/14.
+//  Copyright (c) 2014 Reputation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RPKViewController : UIViewController
+
+@end
---
+++ kiosk/UI/RPKViewController.m
@@ -0,0 +1,25 @@
+//
+//  RPKViewController.m
+//  kiosk
+//
+//  Created by PC Nguyen on 12/16/14.
+//  Copyright (c) 2014 Reputation. All rights reserved.
+//
+
+#import "RPKViewController.h"
+
+@interface RPKViewController ()
+
+@end
+
+@implementation RPKViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
