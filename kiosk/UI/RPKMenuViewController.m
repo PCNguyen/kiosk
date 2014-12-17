b53bdeae6bc5cc98bc5dec8a30c5a9dcd5966964
PC Nguyen <pnguyen@reputation.com>
2014-12-16 16:43:49 -0800

    finish implement Menu Cell

--- kiosk/UI/RPKMenuViewController.m
+++ kiosk/UI/RPKMenuViewController.m
@@ -13,6 +13,8 @@
 
 #import "UIImage+RPK.h"
 
+#pragma mark -
+
 /********************************
  *  RPKMenuCell
  ********************************/
@@ -41,13 +43,95 @@
 - (void)layoutSubviews
 {
 	[super layoutSubviews];
+	
+	self.largeTitleLabel.frame = [self largeTitleLabelFrame];
+	self.logoImageView.frame = [self logoImageFrame:self.largeTitleLabel.frame];
+	self.detailLabel.frame = [self detailLabelFrame:self.logoImageView.frame];
 }
+
+#pragma mark - Override
 
 - (void)assignModel:(id)model forIndexPath:(NSIndexPath *)indexPath
 {
 	RPKMenuItem *menuItem = (RPKMenuItem *)model;
 	
 	self.logoImageView.image = [UIImage rpk_bundleImageNamed:menuItem.imageName];
+	self.largeTitleLabel.text = menuItem.itemTitle;
+	self.detailLabel.text = menuItem.itemDetail;
+}
+
+#pragma mark - Title
+
+- (CGRect)largeTitleLabelFrame
+{
+	CGFloat xOffset = self.paddings.left;
+	CGFloat yOffset = self.paddings.top;
+	CGFloat width = self.bounds.size.width - xOffset - self.paddings.right;
+	CGFloat height = [self.largeTitleLabel sizeThatFits:CGSizeMake(width, MAXFLOAT)].height;
+	
+	return CGRectMake(xOffset, yOffset, width, height);
+}
+
+- (UILabel *)largeTitleLabel
+{
+	if (!_largeTitleLabel) {
+		_largeTitleLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+		_largeTitleLabel.textAlignment = NSTextAlignmentLeft;
+		_largeTitleLabel.textColor = [UIColor darkGrayColor];
+		_largeTitleLabel.font = [UIFont boldSystemFontOfSize:30.0f];
+		_largeTitleLabel.numberOfLines = 1;
+		_largeTitleLabel.adjustsFontSizeToFitWidth = YES;
+	}
+	
+	return _largeTitleLabel;
+}
+
+#pragma mark - Logo Image
+
+- (CGRect)logoImageFrame:(CGRect)topFrame
+{
+	CGFloat xOffset = topFrame.origin.x;
+	CGFloat yOffset = topFrame.origin.y + topFrame.size.height + self.spacings.height;
+	CGFloat height = self.bounds.size.height - yOffset - self.paddings.bottom;
+	CGFloat width = height;
+	
+	return CGRectMake(xOffset, yOffset, width, height);
+}
+
+- (UIImageView *)logoImageView
+{
+	if (!_logoImageView) {
+		_logoImageView = [[UIImageView alloc] initWithFrame:CGRectZero];
+		_logoImageView.contentMode = UIViewContentModeScaleAspectFit;
+	}
+	
+	return _logoImageView;
+}
+
+#pragma mark - Detail Label
+
+- (CGRect)detailLabelFrame:(CGRect)leftFrame
+{
+	CGFloat xOffset = leftFrame.origin.x + leftFrame.size.width + self.spacings.width;
+	CGFloat yOffset = leftFrame.origin.y;
+	CGFloat width = self.bounds.size.width - xOffset - self.paddings.right;
+	CGFloat height = leftFrame.size.height;
+	
+	return CGRectMake(xOffset, yOffset, width, height);
+}
+
+- (UILabel *)detailLabel
+{
+	if (!_detailLabel) {
+		_detailLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+		_detailLabel.textAlignment = NSTextAlignmentLeft;
+		_detailLabel.font = [UIFont systemFontOfSize:14.0f];
+		_detailLabel.numberOfLines = 0;
+		_detailLabel.lineBreakMode = NSLineBreakByWordWrapping;
+		_detailLabel.textColor = [UIColor lightGrayColor];
+	}
+	
+	return _detailLabel;
 }
 
 @end
