c1ce4f3f95028fd6657934249383db721d0e0ef6
PC Nguyen <pnguyen@reputation.com>
2014-12-17 13:43:49 -0800

    handle worse case scenario

--- kiosk/UI/RPKKioskViewController.m
+++ kiosk/UI/RPKKioskViewController.m
@@ -15,6 +15,13 @@
 
 @implementation RPKKioskViewController
 
++ (void)initialize {
+	NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+	[dictionary setObject:@"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A"
+				   forKey:@"UserAgent"];
+	[[NSUserDefaults standardUserDefaults] registerDefaults:dictionary];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 	
@@ -23,6 +30,7 @@
 	self.showActionButton = NO;
 	
 	self.webView.backgroundColor = [UIColor ul_colorWithR:12.0f G:79.0f B:120.0f A:1.0f];
+	self.webView.scalesPageToFit = YES;
 }
 
 - (void)viewWillDisappear:(BOOL)animated
@@ -44,8 +52,26 @@
 {
 	[super webViewDidFinishLoad:webView];
 	
-	NSString *zoomScript = [NSString stringWithFormat:@"window.parent.document.body.style.zoom = 1.5;"];
-	[webView stringByEvaluatingJavaScriptFromString:zoomScript];
+	[[NSUserDefaults standardUserDefaults] setInteger:0 forKey:@"WebKitCacheModelPreferenceKey"];
+	
+	if (!webView.isLoading) {
+		NSLog(@"%@",webView.request);
+		__weak RPKKioskViewController *selfPointer = self;
+		[self performSelector:@selector(centerZoom) withObject:selfPointer afterDelay:5.0f];
+
+	}
+}
+
+- (void)centerZoom
+{
+	CGSize dialogSize = CGSizeMake(560.0f, 420.0f);
+	CGSize scrollViewSize = self.view.bounds.size;
+	CGFloat xOffset = (scrollViewSize.width - dialogSize.width) / 2;
+	CGFloat yOffset = (scrollViewSize.height - dialogSize.height) / 2;
+
+	CGRect zoomRect = CGRectMake(xOffset, yOffset, dialogSize.width, dialogSize.height);
+	[self.webView.scrollView zoomToRect:zoomRect animated:YES];
+
 }
 
 @end
--- kiosk/UI/RPKMenuDataSource.m
+++ kiosk/UI/RPKMenuDataSource.m
@@ -30,6 +30,7 @@
 	RPKMenuItem *googlePlusItem = [[RPKMenuItem alloc] init];
 	NSString *redirectURL = @"https://plus.google.com/_/widget/render/localreview?hl=en&origin=https%3A%2F%2Fplus.google.com&placeid=2221140452266056572&source=lo-pp&jsh=m%3B%2F_%2Fscs%2Fapps-static%2F_%2Fjs%2Fk%3Doz.gapi.en.iBmVlTSSvHg.O%2Fm%3D__features__%2Frt%3Dj%2Fd%3D1%2Ft%3Dzcms%2Frs%3DAGLTcCNDdqzVHtQZOJq6TKMfnKZKIZX46Q#rpctoken=359128756&_methods=onSuccess%2ConCancel%2ConError%2C_ready%2C_close%2C_open%2C_resizeMe%2C_renderstart&id=I3_1418679196082&parent=https%3A%2F%2Fplus.google.com&pfname=";
 	NSString *loginURL = [NSString stringWithFormat:@"https://accounts.google.com/ServiceLogin?passive=1209600&continue=%@", [self urlEncodeString:redirectURL]];
+	loginURL = [NSString stringWithFormat:@"https://plus.google.com/116752878282870811079/about?review=1"];
 	googlePlusItem.itemURL = [NSURL URLWithString:loginURL];
 	googlePlusItem.imageName = @"icon_gplus.png";
 	googlePlusItem.itemTitle = @"Google Review";
