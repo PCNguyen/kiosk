c6913e9471f19abe702a14b532c382ada84977ed
PC Nguyen <pnguyen@reputation.com>
2015-01-07 11:40:07 -0800

    implement countdown timer

--- kiosk/UI/RPKExpirationView.m
+++ kiosk/UI/RPKExpirationView.m
@@ -6,18 +6,73 @@
 //  Copyright (c) 2015 Reputation. All rights reserved.
 //
 
+#import <AppSDK/AppLibScheduler.h>
+
 #import "RPKExpirationView.h"
+
+@interface RPKExpirationView ()
+
+@property (nonatomic, strong) UILabel *countdownLabel;
+@property (nonatomic, strong) ALScheduledTask *countDownTask;
+
+@end
 
 @implementation RPKExpirationView
 
+#pragma mark - View Life Cycle
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+	if (self = [super initWithFrame:frame]) {
+		[self addSubview:self.countdownLabel];
+		[self addConstraints:[self.countdownLabel ul_centerAlignWithView:self]];
+	}
+	
+	return self;
+}
+
+#pragma mark - Label
+
+- (UILabel *)countdownLabel
+{
+	if (!_countdownLabel) {
+		_countdownLabel = [[UILabel alloc] init];
+		_countdownLabel.font = [UIFont boldSystemFontOfSize:40.0f];
+		_countdownLabel.textColor = [UIColor redColor];
+	}
+	
+	return _countdownLabel;
+}
+
+- (void)refreshLabel
+{
+	self.countdownLabel.text = [NSString stringWithFormat:@"Auto Logout In %.0f seconds", self.timeRemaining];
+}
+
+#pragma mark - Count Down
+
 - (void)startCountDown
 {
-
+	[self.countDownTask start];
 }
 
 - (void)stopCountDown
 {
+	[self.countDownTask stop];
+}
 
+- (ALScheduledTask *)countDownTask
+{
+	if (!_countDownTask) {
+		__weak RPKExpirationView *selfPointer = self;
+		_countDownTask = [[ALScheduledTask alloc] initWithTaskInterval:1 taskBlock:^{
+			selfPointer.timeRemaining--;
+			[selfPointer refreshLabel];
+		}];
+		_countDownTask.startImmediately = NO;
+	}
+	
+	return _countDownTask;
 }
 
 @end
--- kiosk/UI/RPKView.h
+++ kiosk/UI/RPKView.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "RPKUIKit.h"
 
 @interface RPKView : UIView
 
