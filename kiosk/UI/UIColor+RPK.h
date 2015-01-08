e9c8905c2faeb3157f2071c2503ae7317866a3ea
PC Nguyen <pnguyen@reputation.com>
2015-01-08 14:14:54 -0800

    adding theming

---
+++ kiosk/UI/UIColor+RPK.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+RPK.h
+//  kiosk
+//
+//  Created by PC Nguyen on 1/8/15.
+//  Copyright (c) 2015 Reputation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (RPK)
+
+@end
---
+++ kiosk/UI/UIColor+RPK.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+RPK.m
+//  kiosk
+//
+//  Created by PC Nguyen on 1/8/15.
+//  Copyright (c) 2015 Reputation. All rights reserved.
+//
+
+#import "UIColor+RPK.h"
+
+@implementation UIColor (RPK)
+
+@end
