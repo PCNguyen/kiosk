ef02d430a22d4400bf2e8972209f0842d0b9aee5
PC Nguyen <pnguyen@reputation.com>
2015-01-19 08:37:39 -0800

    adding login view controller

---
+++ kiosk/UI/RPKLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  RPKLoginViewController.h
+//  Kiosk
+//
+//  Created by PC Nguyen on 1/19/15.
+//  Copyright (c) 2015 Reputation. All rights reserved.
+//
+
+#import "RPKViewController.h"
+
+@interface RPKLoginViewController : RPKViewController
+
+@end
---
+++ kiosk/UI/RPKLoginViewController.m
@@ -0,0 +1,25 @@
+//
+//  RPKLoginViewController.m
+//  Kiosk
+//
+//  Created by PC Nguyen on 1/19/15.
+//  Copyright (c) 2015 Reputation. All rights reserved.
+//
+
+#import "RPKLoginViewController.h"
+
+@interface RPKLoginViewController ()
+
+@property (nonatomic, strong) UITextField *userIDTextField;
+@property (nonatomic, strong) UITextField *passwordTextField;
+@property (nonatomic, strong) UIButton *loginButton;
+
+@end
+
+@implementation RPKLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
