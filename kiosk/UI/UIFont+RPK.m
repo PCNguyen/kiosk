0664b478d4c2f51fc61e8df46b3377d5d0c3f164
PC Nguyen <pnguyen@reputation.com>
2015-01-20 11:49:20 -0800

    adding font category

---
+++ kiosk/UI/UIFont+RPK.m
@@ -0,0 +1,28 @@
+//
+//  UIFont+RPK.m
+//  Kiosk
+//
+//  Created by PC Nguyen on 1/20/15.
+//  Copyright (c) 2015 Reputation. All rights reserved.
+//
+
+#import "UIFont+RPK.h"
+
+@implementation UIFont (RPK)
+
++ (UIFont *)rpk_fontWithSize:(CGFloat)size
+{
+	return [UIFont fontWithName:@"MuseoSans-300" size:size];
+}
+
++ (UIFont *)rpk_boldFontWithSize:(CGFloat)size
+{
+	return [UIFont fontWithName:@"MuseoSans-500" size:size];
+}
+
++ (UIFont *)rpk_extraBoldFontWithSize:(CGFloat)size
+{
+	return [UIFont fontWithName:@"MuseoSans-700" size:size];
+}
+
+@end
