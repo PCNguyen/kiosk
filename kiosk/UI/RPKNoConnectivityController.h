7898dfb14a061f10a86efabab26da8fd7a1bcd38
PC Nguyen <pnguyen@reputation.com>
2015-01-30 15:31:24 -0800

    adding no connectiivity controller

---
+++ kiosk/UI/RPKNoConnectivityController.h
@@ -0,0 +1,13 @@
+//
+//  RPKNoConnectivityController.h
+//  Kiosk
+//
+//  Created by PC Nguyen on 1/30/15.
+//  Copyright (c) 2015 Reputation. All rights reserved.
+//
+
+#import "RPKViewController.h"
+
+@interface RPKNoConnectivityController : RPKViewController
+
+@end
