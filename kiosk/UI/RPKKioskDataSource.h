259d243e697403c7fde3f49cdbd283d0204739ff
PC Nguyen <pnguyen@reputation.com>
2015-02-04 09:32:34 -0800

    adding kioskDataSource

---
+++ kiosk/UI/RPKKioskDataSource.h
@@ -0,0 +1,15 @@
+//
+//  RPKKioskDataSource.h
+//  Kiosk
+//
+//  Created by PC Nguyen on 2/4/15.
+//  Copyright (c) 2015 Reputation. All rights reserved.
+//
+
+#import "RPPreferenceDataSource.h"
+
+@interface RPKKioskDataSource : RPPreferenceDataSource
+
+@property (nonatomic, strong) NSNumber *kioskLocked;
+
+@end
