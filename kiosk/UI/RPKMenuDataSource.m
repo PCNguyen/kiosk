fec97060cb0ef7c57ec8df4f9c02d224f7968703
Peter Fong <feilong7777@Hotmail.com>
2015-12-21 17:56:32 -0800

    debug logs

--- kiosk/UI/RPKMenuDataSource.m
+++ kiosk/UI/RPKMenuDataSource.m
@@ -23,6 +23,7 @@
 {
 	//--grabing the selected location
 	NSArray *locations = [[self preferenceStorage] allLocations];
+
 	self.selectedLocationID = [[self preferenceStorage] loadSelectedLocation];
 	NSString *googleSourceId = [[self preferenceStorage] getGoogleSourceId];
 
@@ -38,7 +39,7 @@
 	} else if ([locations count] == 1) {
 		locationData = [locations firstObject];
 	}
-
+    NSLog(@"locations %@", locationData);
 	if(locationData) {
 		kioskURLString = locationData.kioskUrl;
 		if ([locationData.sourceUrls count] > 0) {
@@ -77,6 +78,8 @@
 	//--loading Google Plus
 	if ([googleURLString length] > 0) {
 		RPKMenuItem *googlePlusItem = [[RPKMenuItem alloc] init];
+        
+       // NSLog(@"google url %@", locationData.sourceUrls);
 
 		NSString *googleURL = [NSString stringWithFormat:@"%@?review=1", @"https://www.google.com/search?q=Amami%20Sushi%2C%201789%20El%20Camino%20Real%2C%20San%20Bruno%2C%20CA%2094066&ludocid=9429747110387290923&rlst=n&ved=1t%3A10503&ei=JnpfVtefE8qW0gS0wbnIBw&hl=en-US#lrd=0x808f7779ed90be7d:0x82dd31093466932b,2"];
 		NSString *loginURL = [NSString stringWithFormat:@"https://accounts.google.com/ServiceLogin?passive=1209600&continue=%@", [self urlEncodeString:googleURL]];
